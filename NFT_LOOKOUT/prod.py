fac703848218750ca689cd4f9d2581cb7d3c2535
s13g <ayflix0@gmail.com>
2022-08-31 19:11:07 +0100

    final commit made

--- NFT_LOOKOUT/prod.py
+++ NFT_LOOKOUT/prod.py
@@ -1,18 +1,16 @@
 from .settings import *
 import os
+from decouple import config
 import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nft-lookup.herokuapp.com']
+ALLOWED_HOSTS = ['nfts-lookup.herokuapp.com']
 
 DATABASES = {
     "default": dj_database_url.config()
 }
-
-
-
